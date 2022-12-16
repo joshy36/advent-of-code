84336836c881eb741171e40b566ae4650714e822
Joshua Bender <36bend@gmail.com>
2022-12-16 14:24:08 -0500

    simpler file read

--- day1/solution.js
+++ day1/solution.js
@@ -1,30 +1,28 @@
 const fs = require('fs');
 
-fs.readFile('data.txt', 'utf8', (err, data) => {
-  if (err) throw err;
+let data = fs.readFileSync('data.txt', 'utf8');
 
-  // Part 1
-  const split = data.split('\n\n');
-  const mapSplit = split.map((x) => x.split('\n'));
-  const numbers = mapSplit.map((x) => x.map(Number));
-  const sum = numbers.map((x) =>
-    x.reduce((accumulator, value) => {
-      return accumulator + value;
-    }, 0)
-  );
-  const max = Math.max(...sum);
+// Part 1
+const split = data.split('\n\n');
+const mapSplit = split.map((x) => x.split('\n'));
+const numbers = mapSplit.map((x) => x.map(Number));
+const sum = numbers.map((x) =>
+  x.reduce((accumulator, value) => {
+    return accumulator + value;
+  }, 0)
+);
+const max = Math.max(...sum);
 
-  console.log(max);
+console.log(max);
 
-  // Part 2
-  let topThree = max;
+// Part 2
+let topThree = max;
+sum.splice(sum.indexOf(max), 1);
+
+for (let i = 0; i < 2; i++) {
+  let max = Math.max(...sum);
+  topThree += max;
   sum.splice(sum.indexOf(max), 1);
+}
 
-  for (let i = 0; i < 2; i++) {
-    let max = Math.max(...sum);
-    topThree += max;
-    sum.splice(sum.indexOf(max), 1);
-  }
-
-  console.log(topThree);
-});
+console.log(topThree);
