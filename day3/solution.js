d80b7bbcf26e95b311266f081cc6f8f987ad35df
Joshua Bender <36bend@gmail.com>
2022-12-20 12:33:08 -0500

    day3

--- day3/solution.js
+++ day3/solution.js
@@ -21,3 +21,21 @@ for (let i = 0; i < split.length; i++) {
 }
 
 console.log(sum);
+
+// Part 2
+let sum2 = 0;
+for (let i = 0; i < split.length; i += 3) {
+  for (let j = 0; j < split[i].length; j++) {
+    let regex = new RegExp(split[i][j]);
+    if (regex.test(split[i + 1]) && regex.test(split[i + 2])) {
+      if (split[i].charCodeAt(j) < 97) {
+        sum2 += split[i].charCodeAt(j) - 38;
+      } else {
+        sum2 += split[i].charCodeAt(j) - 96;
+      }
+      j = split[i].length;
+    }
+  }
+}
+
+console.log(sum2);
