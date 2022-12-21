23639ef1603e7106c86c6e62685095672e952ff1
Joshua Bender <36bend@gmail.com>
2022-12-21 16:31:27 -0500

    day6

--- day6/solution.js
+++ day6/solution.js
@@ -3,39 +3,23 @@ const fs = require('fs');
 const data = fs.readFileSync('data.txt', 'utf8');
 
 // Part 1
-const marker = 4;
-let count = 0;
-
-for (let i = 0; i < data.length; i++) {
-  for (let j = marker - 1; j > 0; j--) {
-    for (let k = j - 1; k >= 0; k--) {
-      if (data[i + j] == data[i + k]) {
-        count++;
-      }
-    }
-  }
-  if (count == 0) {
-    console.log(i + marker);
-    i = data.length;
-  }
-  count = 0;
-}
+console.log(findMarker(data, 4));
 
 // Part 2
-const marker2 = 14;
-let count2 = 0;
+console.log(findMarker(data, 14));
 
-for (let i = 0; i < data.length; i++) {
-  for (let j = marker2 - 1; j > 0; j--) {
-    for (let k = j - 1; k >= 0; k--) {
-      if (data[i + j] == data[i + k]) {
-        count2++;
+function findMarker(data, marker) {
+  for (let i = 0; i < data.length; i++) {
+    let isMarker = true;
+    for (let j = marker - 1; j > 0; j--) {
+      for (let k = j - 1; k >= 0; k--) {
+        if (data[i + j] == data[i + k]) {
+          isMarker = false;
+        }
       }
     }
+    if (isMarker === true) {
+      return i + marker;
+    }
   }
-  if (count2 == 0) {
-    console.log(i + marker2);
-    i = data.length;
-  }
-  count2 = 0;
 }
