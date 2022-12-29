a711b8241c62232abfffd755ca5e2c73c4d7c661
Joshua Bender <36bend@gmail.com>
2022-12-29 18:20:14 -0500

    day9

--- src/day9/solution.ts
+++ src/day9/solution.ts
@@ -3,6 +3,52 @@ import fs from 'fs';
 const data = fs.readFileSync('data.txt', 'utf8');
 const split = data.split('\n');
 
-let map: boolean[][] = [[true, true]];
+let map: number[][] = Array.from(Array(1), () => new Array(1).fill(0));
+let [row, col] = [map.length - 1, 0];
+map[row][col] = 1;
 
-console.log(split);
+for (let i = 0; i < split.length; i++) {
+  const [dir, amount] = split[i].split(' ');
+  if (dir === 'R') {
+    for (let j = 0; j < Number(amount); j++) {
+      col++;
+      map[row][col] = 1;
+    }
+  }
+  if (dir === 'U') {
+    for (let j = 0; j < Number(amount); j++) {
+      console.log(row);
+      console.log(map.length - 1);
+
+      map.unshift(new Array(map[0].length).fill(0));
+      // row--;
+      map[row][col] = 1;
+    }
+  }
+  if (dir === 'L') {
+    for (let j = 0; j < Number(amount); j++) {
+      col--;
+      map[row][col] = 1;
+    }
+  }
+  if (dir === 'D') {
+    for (let j = 0; j < Number(amount); j++) {
+      row++;
+      map[row][col] = 1;
+    }
+  }
+}
+
+console.log(map);
+
+// .HHHH.
+// .HHHHH
+// HHHHHH
+// ....H.
+// HHHHH.
+
+// ..##..
+// ...##.
+// .####.
+// ....#.
+// s###..
