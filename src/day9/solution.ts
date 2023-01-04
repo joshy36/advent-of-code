12f1ceebdc706b46230a4f648358c39f3ac01ef9
Joshua Bender <36bend@gmail.com>
2023-01-03 22:58:16 -0500

    day9

--- src/day9/solution.ts
+++ src/day9/solution.ts
@@ -5,6 +5,8 @@ const split = data.split('\n');
 
 let map: number[][] = Array.from(Array(1), () => new Array(1).fill(0));
 let [row, col] = [map.length - 1, 0];
+let [x, y] = [map.length - 1, 0];
+map[x][y] = 1;
 map[row][col] = 1;
 
 for (let i = 0; i < split.length; i++) {
@@ -13,17 +15,30 @@ for (let i = 0; i < split.length; i++) {
     for (let j = 0; j < Number(amount); j++) {
       col++;
       map[row][col] = 1;
+      for (const r in map) {
+        if (map[r].length < map[row].length) {
+          map[r].push(0);
+        }
+      }
     }
   }
   if (dir === 'U') {
     for (let j = 0; j < Number(amount); j++) {
-      console.log(row);
-      console.log(map.length - 1);
+      console.log('   ', map.length);
+      console.log('row', row + 1);
+      if (row + 1 === map.length) {
+        map.unshift(new Array(map[0].length).fill(0));
+        // console.log(map);
 
-      map.unshift(new Array(map[0].length).fill(0));
+        map[row][col] = 1;
+        row++;
+        console.log(map);
+      }
+      // } else {
       // row--;
-      map[row][col] = 1;
+      // map[row][col] = 1;
     }
+    // }
   }
   if (dir === 'L') {
     for (let j = 0; j < Number(amount); j++) {
